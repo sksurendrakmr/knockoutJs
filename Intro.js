0563547ab1edcabb5cec49770ee9205252680362
sksurendrakmr <sksurendrakmr@gmail.com>
2022-03-23 21:27:10 +0530

    Intro and declare reference to a function

---
+++ Intro.js
@@ -0,0 +1,13 @@
+/**
+ * 1. create a viewModel
+ * ViewModel is an instance of a class that gets bound to chunk of html.
+ * Inside of that class / function, we can have data and that data can then
+ * interact with HTML and HTML can interact with the data.
+ * (This concept is called two-way binding.)
+ */
+
+function CounterViewModel() {
+  //recommended and very common to have reference of this
+  //function level scope.
+  var self = this;
+}
