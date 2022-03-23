293e6edab760860d240003b35619a3e35f2dbb79
sksurendrakmr <sksurendrakmr@gmail.com>
2022-03-24 00:48:28 +0530

    js implementation of reusable component

--- components/index.js
+++ components/index.js
@@ -1,4 +1,48 @@
-function ItemListViewModel() {}
+function ItemListViewModel() {
+  var self = this;
+  self.items = ko.observableArray([]);
+
+  self.fetchItems = function (callback) {
+    setTimeout(function () {
+      var MOCK_RESPONSE = {
+        items: ["I", "Found", "Some", "Items"],
+      };
+      callback(MOCK_RESPONSE.items);
+    }, 400);
+  };
+
+  self.onNewItems = function (newItems) {
+    self.items.push(newItems);
+  };
+}
+
+ko.components.register("loading-button", {
+  // template:'<button></button>' //single line
+  template: [
+    "<button class='LoadingButton' data-bind='click: onClick, css:{loading:isLoading}'>",
+    "<span data-bind='text:buttonText'>Get Items</span>",
+    "</button>",
+  ].join(""),
+  viewModel: function (params) {
+    var self = this;
+    self.buttonText = ko.observable(params.buttonText);
+
+    self.isLoading = ko.observable(false);
+
+    self.onClick = function () {
+      self.isLoading(true);
+      params.action(function (data) {
+        self.isLoading(false);
+        params.onDone(data); //sending data back to parent
+      });
+    };
+  },
+});
 
 const appKnockout = document.querySelector("#knockout-app");
 ko.applyBindings(new ItemListViewModel(), appKnockout);
+
+/**
+ * dynamic css -> css:{"classname":when}
+ * onDone callback we are using -> similar to callback from parent meaning sending some thing to parent
+ */
